475390418abfe4fd4d8107ef88194975513d14c9
Supremeduck008 <pedro.otaviobraga@gmail.com>
2025-04-11 10:34:53 -0300

    Exercício 2 : Números impares de 1 a 15

--- Ex2.js
+++ Ex2.js
@@ -1,6 +1,9 @@
-let numero = 0
+let numero = 0;
 
+console.log("Os números impares de 0 a 15 são:")
 while (numero <= 15){
-    numero++;
+    numero++
+    if (numero % 2 !== 0){
+        console.log(numero)
+    }
 }
-console.log()
