8814cb3ba33efb7410cf8304a8109825ae3ba044
Supremeduck008 <pedro.otaviobraga@gmail.com>
2025-04-11 11:34:26 -0300

    Exercício 7 : moedas

---
+++ Ex7.js
@@ -0,0 +1,9 @@
+let moedas = 3;
+let numerousados= 0;
+
+while(moedas <= 12){
+    console.log(moedas);
+    moedas=moedas+3;
+    numerousados++;
+}
+console.log(`Foram usados ${numerousados} números`);
