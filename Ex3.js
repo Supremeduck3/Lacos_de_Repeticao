73d405b0d7182d6038f0eb5a4f4affe4b34761a5
Supremeduck008 <pedro.otaviobraga@gmail.com>
2025-04-11 10:51:27 -0300

    Exercício 3 : Soma até 40

---
+++ Ex3.js
@@ -0,0 +1,10 @@
+let conta= 1;
+let numero= 1;
+let numerosusados= 0;
+
+console.log(`Seu resultado é:`);
+while (conta <= 40){
+    console.log (conta = conta + conta);
+    numerosusados++;
+}
+console.log(`Foram usados ${numerosusados} números`);
